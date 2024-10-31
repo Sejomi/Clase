18b851bf5785c838a4f220c08aa63053e0278f8a
Sejomi <j.miguelverdu@gmail.com>
2024-10-31 13:53:10 +0100

    hola

--- src/main/java/Programacion/EjerciciosT5/Ej12.java
+++ src/main/java/Programacion/EjerciciosT5/Ej12.java
@@ -8,14 +8,24 @@ public class Ej12 {
         Scanner leer = new Scanner(System.in);
         String genero = null;
         String nombre = null;
+        String grupo = null;
 
         System.out.println("Introduzca su nombre: ");
         nombre = leer.next();
+
         System.out.println("Introduzca su género (M) o (H): ");
         genero = leer.next();
+        while ((genero.toUpperCase().charAt(0) != 'M' && genero.toUpperCase().charAt(0) != 'H') || (genero.length() > 1)) {
+            System.out.println("Introduzca un valor válido: ");
+            genero = leer.next();
 
-        if (genero.toUpperCase().equals("M")) {
         }
-
+        if (genero.toUpperCase().charAt(0) == 'M' && nombre.toUpperCase().charAt(0) <= 'M' || genero.toUpperCase().charAt(0) == 'H' && nombre.toUpperCase().charAt(0) >= 'N') {
+            grupo = "Grupo A";
+        } else {
+            grupo = "Grupo B";
+        }
+        System.out.println("Usted corresponde al: " + grupo);
     }
+
 }
