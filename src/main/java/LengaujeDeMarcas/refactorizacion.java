449ca2b745a57cfaf86e635f52f89b8115beab18
Sejomi <j.miguelverdu@gmail.com>
2024-11-11 12:53:27 +0100

    hola

--- src/main/java/LengaujeDeMarcas/refactorizacion.java
+++ src/main/java/LengaujeDeMarcas/refactorizacion.java
@@ -1,12 +1,15 @@
 package LengaujeDeMarcas;
 
+import java.util.Scanner;
+
 public class refactorizacion {
 
-    public static int fran; {
+    public static int fran() {
         int holi = 41;
+        return holi;
     }
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("41");
 
     }
 }
