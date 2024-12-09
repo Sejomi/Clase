0c381122da533403596df15bb233b7b1aa3799ee
Sejomi <j.miguelverdu@gmail.com>
2024-12-09 13:48:37 +0100

    hola

--- src/main/java/Programacion/Ejercicios6/Ej10.java
+++ src/main/java/Programacion/Ejercicios6/Ej10.java
@@ -1,7 +1,40 @@
 package Programacion.Ejercicios6;
 
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
 public class Ej10 {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
+        float min = 0;
+        int omedia = 0;
+        float max = 0;
+        int umedia = 0;
+        float media = 0;
+        int N = 0;
+        System.out.println("Inrtoduzca un número: ");
+        N = sc.nextInt();
+        float yea[] = new float[N];
+        for (int i = 0; i < N; i++) {
+            System.out.println("Introduzca la altura número " + (i + 1) + ": ");
+            yea[i] = sc.nextFloat();
+            if (i == 0) {
+                min = yea[i];
+                max = yea[i];
+            } else if (yea[i] > max) {
+                max = yea[i];
+            } else if (yea[i] < min) {
+                min = yea[i];
+            } media = media + yea[i];
+        } media = media / N;
+        for (int i = 0; i < N; i++) {
+            if (yea[i] >= media) {
+                omedia++;
+            } else {
+                umedia++;
+            }
+        }
+        System.out.println("Media: " + df.format(media) + "\nMin: " + df.format(min) + "\nMax: " + df.format(max) + "\nBajo la media: " + umedia + "\nSobre la media: " + omedia);
     }
 }
